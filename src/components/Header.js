a70e7586cc43351742ae4dd6cafeacee7502d0f1
Matheus Queiroz <matheus_giga@hotmail.com>
2022-07-08 00:36:16 -0300

    Finalizando o quarto requisito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -27,6 +28,36 @@ class Header extends React.Component {
         Header
         { loading ? <Loading />
           : <p data-testid="header-user-name">{ userName.name }</p> }
+        <nav>
+          <NavLink
+            data-testid="link-to-gome"
+            activeClassName="selected"
+            to="/"
+          >
+            Início
+          </NavLink>
+          <NavLink
+            data-testid="link-to-search"
+            activeClassName="selected"
+            to="/search"
+          >
+            Pesquisar
+          </NavLink>
+          <NavLink
+            data-testid="link-to-favorites"
+            activeClassName="selected"
+            to="/favorites"
+          >
+            Favoritas
+          </NavLink>
+          <NavLink
+            data-testid="link-to-profile"
+            activeClassName="selected"
+            to="/profile"
+          >
+            Perfil
+          </NavLink>
+        </nav>
       </header>
     );
   }
